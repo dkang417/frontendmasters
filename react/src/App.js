203e6866f426650b60d812367ff9cf594b75ec9d
dkang417 <dkang417@gmail.com>
2018-11-20 21:13:45 -0500

    add details and results

--- react/src/App.js
+++ react/src/App.js
@@ -1,65 +1,25 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
-import pf from "petfinder-client";
+import { Router, Link } from "@reach/router";
+import Results from "./results";
+import Details from "./details";
 
-//from our .env file
-const petfinder = pf({
-  key: process.env.API_KEY,
-  secret: process.env.API_SECRET
-});
-
-// class component
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      pets: []
-    };
-  }
-
-  componentDidMount() {
-    petfinder.pet.find({ output: "full", location: "NYC, NY" }).then(data => {
-      let pets;
-
-      if (data.petfinder.pets && data.petfinder.pets.pet) {
-        if (Array.isArray(data.petfinder.pets.pet)) {
-          pets = data.petfinder.pets.pet;
-        } else {
-          pets = [data.petfinder.pets.pet];
-        }
-      } else {
-        pets = [];
-      }
-      this.setState({
-        pets: pets
-      });
-    });
-  }
-
   render() {
     return (
       <div>
-        <h1>Adopt Me Please! </h1>
-        {/* <pre>
-          <code>{JSON.stringify(this.state, null, 4)}</code>
-        </pre> */}
-        <div>
-          {this.state.pets.map(pet => {
-            let breed;
-            if (Array.isArray(pet.breeds.breed)) {
-              breed = pet.breeds.breed.join(",");
-            } else {
-              breed = pet.breeds.breed;
-            }
-            return;
-            <Pet animal={pet.animal} name={pet.name} breed={breed} />;
-          })}
-        </div>
+        <header>
+          {" "}
+          <Link to="/">Adopt me!</Link>
+        </header>
+
+        <Router>
+          <Results path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     );
   }
 }
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
