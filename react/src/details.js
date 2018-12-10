03fd415c669b78c27a266ae756d7fcbf10cba27a
dkang417 <dkang417@gmail.com>
2018-12-09 21:23:40 -0500

    update

--- react/src/details.js
+++ react/src/details.js
@@ -46,7 +46,7 @@ class Details extends React.Component {
 
   render() {
     if (this.state.loading) {
-      return <h1>loading … </h1>;
+      return <h1>not loading lol </h1>;
     }
 
     const {
