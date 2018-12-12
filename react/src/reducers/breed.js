1b4f6b079de097762e4bd7e3cf916e6562f5c528
dkang417 <dkang417@gmail.com>
2018-12-12 14:09:07 -0500

    add breed reducer

---
+++ react/src/reducers/breed.js
@@ -0,0 +1,13 @@
+// reducer is a function
+// takes in old state, action object, returns NEW STATE
+// pure function.  no side effects
+
+export default function breedReducer(state = "", action) {
+  if (action.type === "SET_BREED") {
+    return action.payload;
+  } else if (action.type === "SET_ANIMAL") {
+    return "";
+  } else {
+    return state;
+  }
+}
