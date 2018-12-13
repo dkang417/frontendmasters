4c28d98437b47a677f34f23a6dee3914f13d1cd3
dkang417 <dkang417@gmail.com>
2018-12-12 19:19:30 -0500

    breeds reducer

--- react/src/reducers/breeds.js
+++ react/src/reducers/breeds.js
@@ -2,8 +2,8 @@
 // takes in old state, action object, returns NEW STATE
 // pure function.  no side effects
 
-export default function animalReducer(state = "", action) {
-  if (action.type === "SET_ANIMAL") {
+export default function breedsReducer(state = [], action) {
+  if (action.type === "SET_BREEDS") {
     return action.payload;
   } else {
     return state;
