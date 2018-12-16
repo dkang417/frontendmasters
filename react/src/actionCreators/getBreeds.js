3dfa925543d494bb0ee7074ef360ed0ebbfe5a6e
dkang417 <dkang417@gmail.com>
2018-12-15 22:05:16 -0500

    update

--- react/src/actionCreators/getBreeds.js
+++ react/src/actionCreators/getBreeds.js
@@ -5,4 +5,32 @@ const petfinder = pf({
   secret: process.env.API_SECRET
 });
 
-export default function getBreeds() {}
+export default function getBreeds() {
+  //  the function that returns a function is the thunk
+  return function getBreedsThunk(dispatch, getState) {
+    const { animal } = getState();
+
+    if (animal) {
+      petfinder.breed
+        .list({ animal })
+        .then(data => {
+          if (
+            data.petfinder &&
+            data.petfinder.breeds &&
+            Array.isArray(data.petfinder.breeds.breed)
+          ) {
+            dispatch({
+              type: "SET_BREEDS",
+              payload: data.petfinder.breeds.breed
+            });
+          } else {
+            this.setState({ breeds: [] });
+            dispatch({ type: "SET_BREEDS", payload: [] });
+          }
+        })
+        .catch(console.error);
+    } else {
+      dispatch({ type: "SET_BREEDS", payload: [] });
+    }
+  };
+}
